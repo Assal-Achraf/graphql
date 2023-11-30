0bbbc761b35322e0b3d3543c28aa230d6a162b64
unknown <contact.achrafassal@gmail.com>
2023-11-30 13:09:06 +0000

    Create user schema for graph ql

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
-
+// import { createHandler } from 'graphql-http/lib/use/express';
+const graphql_http= require("graphql-http/lib/use/express");
+const schema = require("./schema/schema")
 const app = express();
 
+app.all('/graphql', graphql_http.createHandler({schema:schema}));
 app.listen(4000, () => {
     console.log('now listening for requests on port 4000');
 });
