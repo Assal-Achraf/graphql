8754a17e5bd8e75ddef050640e766dd488a85c81
unknown <contact.achrafassal@gmail.com>
2023-12-11 15:33:31 +0100

    add tow way's relation ship

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,14 +8,21 @@ const {
     GraphQLString,
     GraphQLSchema,
     GraphQLID,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLList
 } = graphql;
 
+
+var BookType;
+var AuthorType;
 // dummy data
 var books = [
-    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' ,authorId:'1'},
-    { name: 'The Final Empire', genre: 'Fantasy', id: '2' ,authorId:'2'},
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' ,authorId:'3'},
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+    { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+    { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+    { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
 ];
 var authors = [
     { name: 'Patrick Rothfuss', age: 44, id: '1' },
@@ -23,29 +30,38 @@ var authors = [
     { name: 'Terry Pratchett', age: 66, id: '3' }
 ];
 
-const AuthorType =  new GraphQLObjectType({
-    name: 'Author',
-    fields: {
-      id: { type: GraphQLID },
-      name: { type: GraphQLString },
-      age: { type: GraphQLInt }
-    },
-  });
 
- const BookType =  new GraphQLObjectType({
-      name: 'Book',
-      fields: {
+AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: ( ) => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args){
+                return _.filter(books, { authorId: parent.id });
+            }
+        }
+    })
+});
+
+BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: ( ) => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
-        author:{
-            type:AuthorType,
-            resolve(parent,args){
-                return _.find(authors,{id : parent.authorId})
+        author: {
+            type: AuthorType,
+            resolve(parent, args){
+                return _.find(authors, { id: parent.authorId });
             }
         }
-      },
-    });
+    })
+});
+
+
 
 
 
