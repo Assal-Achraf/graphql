6bf84b44ac7bc58b3da6f366116d1acba71e50cb
unknown <contact.achrafassal@gmail.com>
2023-12-12 17:00:48 +0100

    i add a .env file and i change the password for atlas acount hahahah

--- server/app.js
+++ server/app.js
@@ -2,11 +2,22 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose')
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+const a = dotenv.config();
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+const mongodb_username = process.env.MONGODB_USERNAME;
+const mongodb_password = process.env.MONGODB_PASSWORD;
 
-mongoose.connect('mongodb+srv://qasem_lopes:xEEQyckrvZhrUJ2E@cluster0.vfm6iyg.mongodb.net/?retryWrites=true&w=majority')
+const mongodb_cluster = process.env.MONGODB_CLUSTER;
+const mongo_conection_string = `mongodb+srv://${mongodb_username}:${mongodb_password}@${mongodb_cluster}.vfm6iyg.mongodb.net/?retryWrites=true&w=majority`;
+console.log(a);
+console.log(mongo_conection_string)
+mongoose.connect(mongo_conection_string)
 mongoose.connection.once('open', () => {
     console.log('conneted to database');
 });
@@ -17,6 +28,6 @@ app.use('/graphql', graphqlHTTP.graphqlHTTP({
     graphiql: true
 }));
 
-app.listen(4000, () => {
+app.listen(port, () => {
     console.log('now listening for requests on port 4000');
 });
