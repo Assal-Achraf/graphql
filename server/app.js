cb2683dd0f8b571064b3286476d298459a78d788
unknown <contact.achrafassal@gmail.com>
2023-12-12 16:11:39 +0100

    add the logic for returning all the books and else returning all the authors

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose')
 
 const app = express();
+
+
+mongoose.connect('mongodb+srv://qasem_lopes:xEEQyckrvZhrUJ2E@cluster0.vfm6iyg.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connection.once('open', () => {
+    console.log('conneted to database');
+});
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP.graphqlHTTP({
